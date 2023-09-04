f330b968fb5328ff39a47848dba22767e1d3d63d
CloudyErwinter <113581748+CloudyErwinter@users.noreply.github.com>
2023-09-04 11:50:18 +0700

    erwin

--- js/main.js
+++ js/main.js
@@ -1,23 +1,12 @@
-var dropdownButton = document.getElementById("dropdown-button");
-var dropdownContent = document.getElementById("dropdown-content");
-
-dropdownButton.addEventListener("click", function () {
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-});
-
-window.addEventListener("click", function (event) {
-    if (!dropdownButton.contains(event.target) && !dropdownContent.contains(event.target)) {
-        dropdownContent.style.display = "none";
-    }
-});
-
 const menuToggle = document.querySelector('.menu-toggle input');
 const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function( ) {
     nav.classList.toggle('slide');
 });
+
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu")
+}
