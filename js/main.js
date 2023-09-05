a02344d4fad51bf0292eefee5f83ef42e80645bf
CloudyErwinter <113581748+CloudyErwinter@users.noreply.github.com>
2023-09-06 00:15:59 +0700

    Erwin

--- js/main.js
+++ js/main.js
@@ -10,3 +10,9 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu() {
     subMenu.classList.toggle("open-menu")
 }
+const activePage = window.location.pathname;
+console.navLinks = document.querySelectorAll('nav a').forEach(link => {
+    if(link.href.includes(`${activePage}`)) {
+        link.classList.add('active');
+    }
+})
