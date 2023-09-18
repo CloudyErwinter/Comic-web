e8367602a25f3f8456f447075d9b5e279eaabebc
CloudyErwinter <113581748+CloudyErwinter@users.noreply.github.com>
2023-09-18 16:31:52 +0700

    ers

--- js/main.js
+++ js/main.js
@@ -16,3 +16,40 @@ console.navLinks = document.querySelectorAll('nav ul li a').forEach(link => {
         link.classList.add('active');
     }
 })
+let slideIndex = 0;
+
+function showSlides() {
+  const slides = document.querySelectorAll(".slide");
+  slideIndex++;
+
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+
+  const translateXValue = -slideIndex * 100;
+  document.querySelector(".carousel").style.transform = `translateX(${translateXValue}%)`;
+}
+
+function prevSlide() {
+  slideIndex--;
+
+  if (slideIndex < 0) {
+    slideIndex = 2; // Adjust the number based on the number of slides
+  }
+
+  showSlides();
+}
+
+function nextSlide() {
+  slideIndex++;
+
+  if (slideIndex >= 3) { // Adjust the number based on the number of slides
+    slideIndex = 0;
+  }
+
+  showSlides();
+}
+
+// Automatically advance the slides every 3 seconds
+setInterval(nextSlide, 3000); // Adjust the interval as needed
+  
