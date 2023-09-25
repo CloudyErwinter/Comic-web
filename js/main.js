d16eddd55ae7bf76e38e7efb82577f07a46c507e
CloudyErwinter <113581748+CloudyErwinter@users.noreply.github.com>
2023-09-25 15:05:21 +0700

    erwin

--- js/main.js
+++ js/main.js
@@ -16,40 +16,72 @@ console.navLinks = document.querySelectorAll('nav ul li a').forEach(link => {
         link.classList.add('active');
     }
 })
-let slideIndex = 0;
 
-function showSlides() {
-  const slides = document.querySelectorAll(".slide");
-  slideIndex++;
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const arrowBtn = document.querySelectorAll(".wrapper i");
+const firstCardsWidth = carousel.querySelector(".cards").offsetWidth;
+// const carouselChildrens = [...carousel.children];
 
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
+let isDragging = false, startX, startScrollLeft, timeoutId;
+
+// get number of the card that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardsWidth);
+
+// // insert copies of the last few cards to beginning of the carousel for infinite scrolling
+// carouselChildrens.slice(-cardPerView).reverse().forEach(cards => {
+//   carousel.insertAdjacentHTML("afterbegin", cards.outerHTML);
+// });
+// // insert copies of the first few cards to end of the carousel for infinite scrolling
+// carouselChildrens.slice(0, -cardPerView).forEach(cards => {
+//   carousel.insertAdjacentHTML("beforeend", cards.outerHTML);
+// });
+
+// add event listeners for the arrow buttons to scroll to the carousel left and right
+arrowBtn.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardsWidth : firstCardsWidth;
+  })
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+  if(!isDragging) return; // if isDragging is false return from here
+  // update the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX- startX);
+}
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+}
+const autoPlay = () => {
+  if(window.innerWidth < 800) return; // kembali ketika layar lebih kecil dari 800
+  // autoplay the carousel after 2500ms
+  timeoutId = setTimeout(() => carousel.scrollLeft += firstCardsWidth, 2500);
+}
+autoPlay();
+const infiniteScroll = () => {
+  if(carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - (1 * carousel.offsetWidth);
+    carousel.classList.remove("no-transition");
+  } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
   }
 
-  const translateXValue = -slideIndex * 100;
-  document.querySelector(".carousel").style.transform = `translateX(${translateXValue}%)`;
+  clearTimeout(timeoutId);
+  if(!wrapper.matches(":hover")) autoPlay();
 }
 
-function prevSlide() {
-  slideIndex--;
-
-  if (slideIndex < 0) {
-    slideIndex = 2; // Adjust the number based on the number of slides
-  }
-
-  showSlides();
-}
-
-function nextSlide() {
-  slideIndex++;
-
-  if (slideIndex >= 3) { // Adjust the number based on the number of slides
-    slideIndex = 0;
-  }
-
-  showSlides();
-}
-
-// Automatically advance the slides every 3 seconds
-setInterval(nextSlide, 3000); // Adjust the interval as needed
-  
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
